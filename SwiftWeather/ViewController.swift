fcce5999ec5167f26422e5859004a3d3de4d7e0f
Marshall Upshur <marshall@mbucreations.com>
2014-10-06 15:07:27 -0700

    Initial Commit

---
+++ SwiftWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWeather
+//
+//  Created by Marshall Upshur on 10/6/14.
+//  Copyright (c) 2014 Marshall Upshur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
