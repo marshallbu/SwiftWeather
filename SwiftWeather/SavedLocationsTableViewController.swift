bd6a4eb079a347cae9b7823f7da72162d23b8542
Marshall Upshur <marshall@mbucreations.com>
2014-10-06 21:37:43 -0700

    another commit working through this class

---
+++ SwiftWeather/SavedLocationsTableViewController.swift
@@ -0,0 +1,104 @@
+//
+//  SavedLocationsTableViewController.swift
+//  SwiftWeather
+//
+//  Created by Marshall Upshur on 10/6/14.
+//  Copyright (c) 2014 Marshall Upshur. All rights reserved.
+//
+
+import UIKit
+
+class SavedLocationsTableViewController: UITableViewController {
+    
+    // a property
+    var savedLocations = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        savedLocations = ["Sacremento", "Chicago", "Atlanta", "Portland"]
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return savedLocations.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("savedLocationCell", forIndexPath: indexPath) as UITableViewCell
+
+        // Configure the cell...
+        cell.textLabel?.text = savedLocations[indexPath.row]
+
+        return cell
+    }
+    
+    
+    // TODO: come back to this to explore further!
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SwiftWeather/ViewController.swift
+++ SwiftWeather/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationsButton: UIButton!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var currentTemperature: UILabel!
+    @IBOutlet weak var currentWeather: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationsButton.layer.cornerRadius = 7
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
